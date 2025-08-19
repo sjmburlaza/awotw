635e62c0f7558f4c64d8294e9b3fcf8b67b0d3c8
sjmburlaza <hellosharmane@gmail.com>
2025-08-19 22:40:43 +0800

    Implement search functionality and results page

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,34 +1,67 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
+import { FormsModule } from "@angular/forms";
+
+const URL = {
+  HOME: '/home',
+  QUIZ: '/quiz',
+  SEARCH: '/search'
+}
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header implements OnInit {
+  readonly URL = URL;
+  currentUrl = URL.HOME;
   isDarkMode = false;
-  currentUrl = '/home';
+  isHomeClicked = false;
+  zoomInText = false;
+  searchQuery = '';
 
-  constructor( private router: Router ) {}
+  constructor(
+    private router: Router,
+  ) {}
 
   ngOnInit() {
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe((event: any) => {
         this.currentUrl = event.urlAfterRedirects;
-      });
+        if (this.currentUrl === URL.HOME) {
+          this.zoomInText = true;
+        } else {
+          this.zoomInText = false;
+        }
+        if (!event.url.includes(URL.SEARCH)) {
+          this.searchQuery = '';
+        }
+    });
   }
 
-  goToHomePage() {
-    this.router.navigate(['/home']);
+  onSearch(): void {
+    if (this.searchQuery) {
+      this.router.navigate([URL.SEARCH], { queryParams: { q: this.searchQuery }});
+    } else {
+      this.router.navigate([URL.HOME]);
+    }
   }
 
-  goToQuizPage() {
-    this.router.navigate(['/quiz']);
+  goToHomePage(): void {
+    this.isHomeClicked = true;
+    setTimeout(() => {
+      this.router.navigate([URL.HOME]);
+      this.isHomeClicked = false;
+    }, 800);
+  }
+
+  goToQuizPage(): void {
+    this.router.navigate([URL.QUIZ]);
   }
 
   onToggleBgMode(): void {
