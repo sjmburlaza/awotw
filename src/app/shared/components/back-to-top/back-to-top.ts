dabb4530dd17523ae02cfa4955c1e6299a9e51b5
sjmburlaza <hellosharmane@gmail.com>
2025-08-24 23:32:45 +0800

    Initial tooltip implementation, update some components styling

---
+++ src/app/shared/components/back-to-top/back-to-top.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-back-to-top',
+  imports: [],
+  templateUrl: './back-to-top.html',
+  styleUrl: './back-to-top.scss'
+})
+export class BackToTop {
+
+}
--- src/app/timeline/timeline.ts
+++ src/app/timeline/timeline.ts
@@ -4,10 +4,11 @@ import { take } from 'rxjs';
 import { groupByYearBuilt } from '../shared/utils-helper';
 import { animate, style, transition, trigger } from '@angular/animations';
 import { Loader } from '../shared/components/loader/loader';
+import { Tooltip } from '../shared/components/tooltip/tooltip';
 
 @Component({
   selector: 'app-timeline',
-  imports: [Loader],
+  imports: [Loader, Tooltip],
   templateUrl: './timeline.html',
   styleUrl: './timeline.scss'
 })
