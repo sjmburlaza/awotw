7dd7bc2747e5923c5daed9c5fadf3388e71de6f8
sjmburlaza <hellosharmane@gmail.com>
2025-09-09 00:02:09 +0800

    Updating some styling and formatting

--- src/app/location/location.ts
+++ src/app/location/location.ts
@@ -24,14 +24,14 @@ export class Location {
   ) {}
 
   ngOnInit(): void {
-      this.dataService.getWonders().pipe(take(1)).subscribe((res: Item[]) => {
-        const groups = sortAlphabetical(res, 'continent');
-        this.groups = groupByAttribute(groups, 'continent');
-        this.loading = false;
-      });
-  
-      this.activatedRoute.fragment.subscribe((fragment: string | null) => {
-        if (fragment) this.scrollService.scrollToFragment(fragment, 50);
-      });
-    }
+    this.dataService.getWonders().pipe(take(1)).subscribe((res: Item[]) => {
+      const groups = sortAlphabetical(res, 'continent');
+      this.groups = groupByAttribute(groups, 'continent');
+      this.loading = false;
+    });
+
+    this.activatedRoute.fragment.subscribe((fragment: string | null) => {
+      if (fragment) this.scrollService.scrollToFragment(fragment, 50);
+    });
+  }
 }
